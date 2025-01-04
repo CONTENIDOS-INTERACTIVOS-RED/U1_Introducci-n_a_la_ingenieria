079aec0da18f756b2d2a7e986d228544775c4358
Daniel Alvarez <ingdanielf8@gmail.com>
2025-01-04 10:00:36 -0500

    Listo cuestionario

--- src/views/curso/Tema2.vue
+++ src/views/curso/Tema2.vue
@@ -444,6 +444,10 @@
             figure
               img(src='@/assets/componentes/material-complementario.svg', alt='Imagen de material complementario')
 
+    .bg-full-width.border-top.actividad(style="background-color: #ebf1f5; border-top: 5px solid #f5c145 !important")
+      .p-4.p-md-5
+        #Actividad                
+          <Actividad :cuestionario="cuestionario"/>
 
 </template>
 
@@ -454,6 +458,169 @@ export default {
   components: {
     BannerInterno,
   },
+  data: () => ({
+    cuestionario: {
+      tema: 'Fundamentos de Ingeniería',
+      titulo: 'Ponte a prueba',
+      introduccion:
+        'Demuestra lo que aprendiste en esta unidad y pon a prueba tus conocimientos.',
+      barajarPreguntas: true,
+      preguntas: [
+        {
+          id: 1,
+          texto:
+            '¿Cuál de las siguientes opciones mejor describe la ingeniería?',
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'Una ciencia exacta que se basa únicamente en cálculos',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto:
+                'Una disciplina que combina conocimientos teóricos y prácticos',
+              esCorrecta: true,
+            },
+            {
+              id: 'c',
+              texto: 'Una actividad artística que se enfoca en la creatividad',
+              esCorrecta: false,
+            },
+            {
+              id: 'd',
+              texto: 'Una profesión que solo se ocupa de construir edificios',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 2,
+          texto: '¿Qué papel juega la creatividad en la ingeniería?',
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'Es una habilidad poco importante',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto: 'Es esencial para encontrar soluciones innovadoras',
+              esCorrecta: true,
+            },
+            {
+              id: 'c',
+              texto: 'Solo es útil en ciertas áreas de la ingeniería',
+              esCorrecta: false,
+            },
+            {
+              id: 'd',
+              texto: 'Se contrapone al pensamiento lógico',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 3,
+          texto: '¿Qué significa pensar como un ingeniero?',
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto:
+                'Ser capaz de resolver problemas de manera lógica y sistemática',
+              esCorrecta: true,
+            },
+            {
+              id: 'b',
+              texto: 'Tener una gran capacidad para memorizar fórmulas',
+              esCorrecta: false,
+            },
+            {
+              id: 'c',
+              texto:
+                'Ser experto en el uso de software de diseño asistido por computadora',
+              esCorrecta: false,
+            },
+            {
+              id: 'd',
+              texto: 'Ser capaz de trabajar solo sin la ayuda de otros',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 4,
+          texto:
+            'La principal diferencia entre un científico y un ingeniero es que:',
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'El científico descubre y el ingeniero aplica',
+              esCorrecta: true,
+            },
+            {
+              id: 'b',
+              texto:
+                'El científico trabaja en un laboratorio y el ingeniero en una obra',
+              esCorrecta: false,
+            },
+            {
+              id: 'c',
+              texto: 'El científico es más creativo y el ingeniero más lógico',
+              esCorrecta: false,
+            },
+            {
+              id: 'd',
+              texto: 'Ambos cumplen las mismas funciones',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 5,
+          texto:
+            'La principal diferencia entre un científico y un ingeniero es que:',
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'El científico descubre y el ingeniero aplica',
+              esCorrecta: true,
+            },
+            {
+              id: 'b',
+              texto:
+                'El científico trabaja en un laboratorio y el ingeniero en el campo',
+              esCorrecta: false,
+            },
+            {
+              id: 'c',
+              texto: 'El científico es más teórico y el ingeniero más práctico',
+              esCorrecta: false,
+            },
+            { id: 'd', texto: 'tienen muchas diferencias', esCorrecta: false },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+      ],
+      mensaje_final_aprobado: '¡Excelente! Ha superado la actividad.',
+      mensaje_final_reprobado:
+        'Le recomendamos volver a revisar el componente formativo e intentar nuevamente la actividad didáctica.',
+    },
+  }),
   mounted() {
     this.$nextTick(() => {
       this.$aosRefresh()
